1968fda82587c2c719a9d8dba6f1437c1375205e
TahsinProduction <khantahsinkabir@gmail.com>
2019-09-05 17:18:05 +0600

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,6 @@ const AboutPage = () => (
     <SEO title="ContactUS" keywords={[`gatsby`, `application`, `react`]} />
     <div className="col-md-3"></div>
     <img src = "https://lh3.googleusercontent.com/66fkMhM37jZ2C1I9HZbIpn_Oebmx_HytEbvFd4HSKjpULIzO5hzh8w2dRBaibstgIIlUYDkeh8g5KAT1-mFV8tPuZu1cT3aiF1ZeaiB67xlbvh7TtlhdkkhnXRtmfAJEUmpsKR7Q" width="100%" height="auto" margin="0 auto" alt="embed" />
-<h1>If you have any query or problem or want to say something to us then email us at (<strong>tahsinproduction1@gmail.com</strong>)</h1><br></br><h1>You will receive reply shortly</h1>
+<h1>If you have any query or problem or want to say something to us then email us at</h1><p>(<strong>tahsinproduction1@gmail.com</strong>)</p><br></br><h1>You will receive reply shortly</h1>
 </Layout>)
 export default AboutPage
